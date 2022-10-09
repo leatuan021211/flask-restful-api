29bec8b168c0a3ada909ea9263482620cc921499
leatuan021211 <93324299+leatuan021211@users.noreply.github.com>
2022-10-09 18:07:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,4 @@ from flask import send_from_directory
 
 if __name__ == "__main__":
     app = create_app()
-    @app.route('/static/<path:path>')
-    def send_static(path):
-        return send_from_directory('static', path)
     app.run(debug=True)
